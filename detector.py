d4d96e11ae6c17bbf1543a4c2c5d63183fc4b80b
billy270 <43737658+billy270@users.noreply.github.com>
2022-04-04 21:57:07 -0500

    Add files via upload
    cascade 3  --> datos realizados con el software 
    cascade 1 --> Rick prueba 
    Stop_data --> Online 
    Stop.py --> online 
    Detector.py ---> Billy

---
+++ detector.py
@@ -0,0 +1,29 @@
+from types import GeneratorType
+import cv2 
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+
+stop_data = cv2.CascadeClassifier('cascade3.xml') 
+
+
+while  True:
+
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    toy = stop_data.detectMultiScale(gray,
+    scaleFactor = 4,
+    minNeighbors = 150)
+
+    for(x,y,w,h) in toy :
+        cv2.rectangle(frame, (x,y), (x+w,y+h),(255,0,0),2)
+        cv2.putText(frame,'stop',(x,y-10),2,0.7,(0,255,0),2,cv2.LINE_AA)
+
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cap.realease()
+cv2.destroyAllWindows()
